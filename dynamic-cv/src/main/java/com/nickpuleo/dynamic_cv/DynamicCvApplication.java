d2e1037bdf2e16b6358c8228c3d0faad1534c386
nfp <nfpuleo@gmail.com>
2025-11-01 22:44:14 -0500

    First push, initializing project

---
+++ dynamic-cv/src/main/java/com/nickpuleo/dynamic_cv/DynamicCvApplication.java
@@ -0,0 +1,13 @@
+package com.nickpuleo.dynamic_cv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicCvApplication.class, args);
+	}
+
+}
---
+++ dynamic-cv/src/test/java/com/nickpuleo/dynamic_cv/DynamicCvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickpuleo.dynamic_cv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
