c4356368845ddaad62ae87231457b992177c0abe
nfp <nfpuleo@gmail.com>
2025-11-03 19:16:59 -0600

    Beginning work on Controllers, pushing before switching to feature branch

--- dynamic-cv-backend/src/main/java/com/nickpuleo/dynamic_cv/controllers/SkillController.java
+++ dynamic-cv-backend/src/main/java/com/nickpuleo/dynamic_cv/controllers/SkillController.java
@@ -1,4 +1,10 @@
 package com.nickpuleo.dynamic_cv.controllers;
 
+import com.nickpuleo.dynamic_cv.models.Skill;
+import com.nickpuleo.dynamic_cv.repositories.SkillRepository;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
 public class SkillController {
 }
