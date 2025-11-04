29ffadb758abe4d5e2087f23b6ca40041f52e35d
nfp <nfpuleo@gmail.com>
2025-11-03 20:51:30 -0600

    Begin controller work, first push to feature branch

--- dynamic-cv-backend/src/main/java/com/nickpuleo/dynamic_cv/controllers/SkillController.java
+++ dynamic-cv-backend/src/main/java/com/nickpuleo/dynamic_cv/controllers/SkillController.java
@@ -4,7 +4,26 @@ import com.nickpuleo.dynamic_cv.models.Skill;
 import com.nickpuleo.dynamic_cv.repositories.SkillRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+import java.util.List;
+
 
 @RestController
+@RequestMapping("/skill")
 public class SkillController {
+
+    private final SkillRepository repo;
+
+
+    // GET http://localhost:8080/api/skill
+    @GetMapping("")
+    public Collection<Skill> getAllSkills() {
+        return lll.getAll();
+    }
+
+
+    //POST
+    @PostMapping("/addskill")
+
+
 }
