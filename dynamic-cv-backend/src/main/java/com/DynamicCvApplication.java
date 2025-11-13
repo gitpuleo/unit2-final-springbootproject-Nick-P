72e1b70d6dc0e8cb9e0f3999133aea15111872d4
nfp <nfpuleo@gmail.com>
2025-11-13 12:37:41 -0600

    re-adding backend to tracking

---
+++ dynamic-cv-backend/src/main/java/com/DynamicCvApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicCvApplication.class, args);
+	}
+
+}
